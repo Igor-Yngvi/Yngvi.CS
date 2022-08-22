b495021e391e9262accb60c05f6fa0d442db289b
Igor-Yngvi <96557071+Igor-Yngvi@users.noreply.github.com>
2022-08-22 17:20:37 -0300

    Add files via upload

---
+++ Hello Word.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("My name is Igor");
+      Console.WriteLine("I'm 17 now ");
+      Console.WriteLine("I'm learning c#");
+      Console.WriteLine("And this is my first program");
+    }
+  }
+}
